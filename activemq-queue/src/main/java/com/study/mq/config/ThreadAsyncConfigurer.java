8305489ba7c526219084c6504733854c12b07fb3
shiboxue <1240613593@qq.com>
2019-09-30 15:53:12 +0800

    12

---
+++ activemq-queue/src/main/java/com/study/mq/config/ThreadAsyncConfigurer.java
@@ -0,0 +1,46 @@
+package com.study.mq.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * springboot里面创建异步线程配置类，
+ * 配置类创建之后，以后再使用@Async创建异步线程就可以按照自己配置来使用了。
+ * 不用担心和spring线程池数量冲突了。
+ * @author kouyy
+ */
+@Configuration
+@EnableAsync
+public class ThreadAsyncConfigurer implements AsyncConfigurer {
+
+    @Bean
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor threadPool = new ThreadPoolTaskExecutor();
+        //设置核心线程数
+        threadPool.setCorePoolSize(10);
+        //设置最大线程数
+        threadPool.setMaxPoolSize(100);
+        //线程池所使用的缓冲队列
+        threadPool.setQueueCapacity(10);
+        //等待任务在关机时完成--表明等待所有线程执行完
+        threadPool.setWaitForTasksToCompleteOnShutdown(true);
+        // 等待时间 （默认为0，此时立即停止），并没等待xx秒后强制停止
+        threadPool.setAwaitTerminationSeconds(60);
+        //  线程名称前缀
+        threadPool.setThreadNamePrefix("MyAsync-");
+        // 初始化线程
+        threadPool.initialize();
+        return threadPool;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return null;
+    }
+}
